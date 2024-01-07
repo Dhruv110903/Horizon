12b4a7be65f56a2a81742bc23a61a04b79ef87b3
Dhruv Vatsa <93207042+Dhruv110903@users.noreply.github.com>
2024-01-07 11:20:30 +0530

    Update verifyToken.js
    you are not authenticated issue

--- backend/utils/verifyToken.js
+++ backend/utils/verifyToken.js
@@ -33,7 +33,7 @@ export const verifyUser = (req, res, next) => {
 
 export const verifyAdmin = (req, res, next) => {
     verifyToken(req, res, next, () => {
-        if (req.user.role === "admin") {
+        if (req.user.id === req.params.id ||  req.user.role === "admin") {  // some changes here , added before or
             next();
         } else {
            return res.status(401).json({ success: false, message: "You are not authorized" });
