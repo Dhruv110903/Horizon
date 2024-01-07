1a612a00571e148f8add99f3dc59605311d841d2
Dhruv Vatsa <93207042+Dhruv110903@users.noreply.github.com>
2024-01-07 10:41:39 +0530

    Update config.js

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,2 +1,3 @@
-// export const BASE_URL= 'http://localhost:4000/api/v1';
-export const BASE_URL= 'https://horizonbackend.onrender/api/v1';
+// To run locally use local host, for deployment use backend url
+// export const BASE_URL= 'http://localhost:4000/api/v1';  
+export const BASE_URL= 'https://horizonbackend.onrender.com/api/v1';
